d01d44d3cb244e626ec0398d4b8aeddeb4f8993f
Dcamy <dbordwel@gmail.com>
2024-04-16 11:29:22 +0700

    Project cleanup and restructure

---
+++ frontend/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.scss';
+
+import AOS from 'aos';
+import { focusHandling } from 'cruip-js-toolkit';
+
+import Home from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import About from './pages/About';
+import PageNotFound from './pages/PageNotFound';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 600,
+      easing: 'ease-out-sine',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+
+        <Route path="/blog-post">
+          <BlogPost />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
